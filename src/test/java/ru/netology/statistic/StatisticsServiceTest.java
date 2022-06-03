2434e414f7b2c13fe5c4c1b01005787a3d41926e
dmitriysokolov94 <ipsokolov2021@gmail.com>
2022-06-03 21:59:11 +0300

    new test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findAbove12() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 12, 6, 11, 11, 12};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
